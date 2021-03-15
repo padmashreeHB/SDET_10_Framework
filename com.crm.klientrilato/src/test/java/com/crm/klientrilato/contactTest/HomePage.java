bfe0d17dc6f951f4b16946d440c43cb6bb51c615
Darshan <Darshan@Darshan-PC>
2021-03-15 15:37:52 +0530

    added home page method

---
+++ com.crm.klientrilato/src/test/java/com/crm/klientrilato/contactTest/HomePage.java
@@ -0,0 +1,20 @@
+package com.crm.klientrilato.contactTest;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+	
+	@Test
+	public void homepage()
+	{
+		System.out.println("Create Home PageTest");
+	}
+	
+	public void homepagecreate(String name)
+	{
+		System.out.println("Home PAge Create");
+		
+		
+	}
+
+}
